0399653c4683550feaccee69e6acc345cb6a918b
eric-hochberger <erichochberger2020@u.northwestern.edu>
2023-06-01 15:15:02 -0400

    edited get_data to output updates to csvs
    Instead of output of get_data being named list it's instead csvs that can then be pasted in google sheets

--- get_data.R
+++ get_data.R
@@ -1,12 +1,17 @@
 
-
+library(purrr)
+library(rvest)
+library(rlang)
 
 #check current version
 league
 
 #update leage streams
 league <- update_streams(league)
-
-#save output to an RDS file
-saveRDS(league, file = "league_jun_1.rds")
+namelist <- names(league)
+#save output to csvs
+for ( i in seq_along(league) ) {
+  filename <- paste0("updates/",names(league)[i], ".csv")
+  write.csv(league[[i]], file = filename, row.names = FALSE)
+}
 
