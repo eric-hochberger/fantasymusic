b95e25920637c9eea111ac892366a0afdaca7d90
eric-hochberger <erichochberger2020@u.northwestern.edu>
2023-11-10 14:43:51 -0500

    Update add_new_teams.R

--- add_new_teams.R
+++ add_new_teams.R
@@ -178,7 +178,7 @@ league <-
   )
 
 # read in new team submissions
-new_teams <- read_csv('/path/to/googleformoutput.csv')
+new_teams <- read_csv('/Users/Eric.Hochberger/Downloads/Fantasy Music League Sign-up 4.csv')
 new_column_names <- c('time_submitted', 'email', 'teamname', 'artist_25k', 'artist_250k_1', 'artist_250k_2', 'artist_250k_3', 'artist_1m')
 colnames(new_teams) <- new_column_names
 new_teams$artist_250k_1
@@ -301,18 +301,52 @@ googlesheets4::sheet_add('https://docs.google.com/spreadsheets/d/1GtLYZJ3OcywTGT
 
 new_team_dfs <- new_league[new_teams]
 
-length(new_team_dfs)
+
+scoring <- tibble::tribble(
+  ~desc, ~scoring,
+  "25k Artist Growth", "=(INDEX(4:4, 1, COUNTA(4:4))-C4)/C4",
+  "250k Artist 1 Growth", "=(INDEX(5:5, 1, COUNTA(5:5))-C5)/C5",
+  "250k Artist 2 Growth", "=(INDEX(6:6, 1, COUNTA(6:6))-C6)/C6",
+  "250k Artist 3 Growth", "=(INDEX(7:7, 1, COUNTA(7:7))-C7)/C7",
+  "1M Artist Growth", "=(INDEX(8:8, 1, COUNTA(8:8))-C8)/C8"
+)
+
+scoring$scoring <- gs4_formula(scoring$scoring)
+
+scoring_sum <- tibble::tribble(
+  ~name, ~avg_formula
+)
+
+new_team_dfs[1]$Eric %>% select(-artist_code)
 
 for (i in 1:length(new_team_dfs)) {
-  googlesheets4::write_sheet(as.data.frame(new_team_dfs[[i]]), 'https://docs.google.com/spreadsheets/d/1GtLYZJ3OcywTGT75LMIXJwogC5CJx_biGl5l0MuVLM4/edit#gid=0', sheet = new_teams[i])
+  googlesheets4::range_write('https://docs.google.com/spreadsheets/d/1GtLYZJ3OcywTGT75LMIXJwogC5CJx_biGl5l0MuVLM4/edit#gid=0', as.data.frame(new_team_dfs[[i]] %>% select(-artist_code)), sheet = new_teams[i], range = "A3:D8")
+
+  googlesheets4::range_write('https://docs.google.com/spreadsheets/d/1GtLYZJ3OcywTGT75LMIXJwogC5CJx_biGl5l0MuVLM4/edit#gid=0', scoring, sheet = new_teams[i], range = "D20", reformat = FALSE)
+
+new_scoring_sum_row <- tibble(
+  name = new_teams[i],
+  avg_formula = paste0('=AVERAGE(', new_teams[i], '!E21:E25)')
+)
+
+scoring_sum <- bind_rows(scoring_sum, new_scoring_sum_row)
 
 }
 
+scoring_sum$avg_formula <- gs4_formula(scoring_sum$avg_formula)
 
+scoring_sheet <- googlesheets4::read_sheet('https://docs.google.com/spreadsheets/d/1GtLYZJ3OcywTGT75LMIXJwogC5CJx_biGl5l0MuVLM4/edit#gid=0', sheet = 'Scoring')
+
+scoring_sheet <- scoring_sheet %>% filter(complete.cases(`Avg % Diff`))
+
+scoring_sum_range <- paste0('B', nrow(scoring_sheet)+2)
+
+googlesheets4::range_write('https://docs.google.com/spreadsheets/d/1GtLYZJ3OcywTGT75LMIXJwogC5CJx_biGl5l0MuVLM4/edit#gid=0', data = scoring_sum, sheet = 'Scoring', range = scoring_sum_range, col_names = FALSE, reformat = FALSE)
 
 
 #transfer update to the google sheet - doesn't work for the new teams quite yet but everything else does
 spotifystreams::update_sheets(list = new_league, updated_coulmn = 4,
                               sheet_id = "1GtLYZJ3OcywTGT75LMIXJwogC5CJx_biGl5l0MuVLM4",
                               auth_token = token)
+
 
